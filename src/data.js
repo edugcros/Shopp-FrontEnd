d87b8afad362c5e95e185c990a176ea104d18b2d
edugcros <grecoeduardo87@gmail.com>
2022-07-04 16:06:12 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,91 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://cloud.leparking-moto.fr/2021/05/07/01/11/honda-vfr-honda-vfr-750_151087815.jpg",
+      title: "PASSION",
+      desc: "¡NO OLVIDES EL ESTILO! OBTÉN UN 30 % DE DESCUENTO PARA LAS NUEVAS LLEGADAS.",
+      bg: "#dddee0",
+    },
+    {
+      id: 2,
+      img: "https://www.tuningblog.eu/wp-content/uploads/2021/07/Harley-Davidson-Night-Rod-Bad-Boy-Customs-Tuning-5.jpg",
+      title: "STYLE",
+      desc: "¡NO OLVIDES EL ESTILO! OBTÉN UN 30 % DE DESCUENTO PARA LAS NUEVAS LLEGADAS.",
+      bg: "#dddee0",
+    },
+    {
+      id: 3,
+      img: "https://www.moto1pro.com/sites/default/files/yamaha-yzfr1-2014-crossplane_0.jpg",
+      title: "MOTORCYCLE TRACK",
+      desc: "¡NO OLVIDES EL ESTILO! OBTÉN UN 30 % DE DESCUENTO PARA LAS NUEVAS LLEGADAS.",
+      bg: "#dddee0",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "http://loversofmoto.com/wp-content/uploads/parser/Husqvarna-TE-350-1990-5.jpg",
+      title: "ENDURO!",
+      cat:"women"
+    },
+    {
+      id: 2,
+      img: "https://lamoto.com.ar/wp-content/uploads/2019/10/ducati-panigale-v4-2020-1slsls.jpg",
+      title: "SUPER SPORT",
+      cat:"coat"
+    },
+    {
+      id: 3,
+      img: "https://www.mundomotero.com/wp-content/uploads/2017/07/MV-Agusta-F4-7-1024x720.jpg",
+      title: "SPORT LOVE",
+      cat:"jeans"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://motopasionstore.com/blog/wp-content/uploads/2019/10/agv_pistagpr-1024x609.jpg",
+    },
+    {
+      id:2,
+      img:"https://http2.mlstatic.com/D_NQ_NP_615006-MLA48642812690_122021-O.webp",
+    },
+    {
+      id:3,
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVtymudQ-L7cklqa96dCVLpIQUzn_8DVSp4-eSRdylhanLLeHJYIk4P6Dn0AJDO1tgdEw&usqp=CAU",
+    },
+    {
+      id:4,
+      img:"https://http2.mlstatic.com/D_NQ_NP_2X_776361-MLA26356047080_112017-F.webp",
+    },
+    {
+      id:5,
+      img:"https://http2.mlstatic.com/D_NQ_NP_706154-MLA49665803564_042022-O.webp",
+    },
+    {
+      id:6,
+      img:"https://blog.nilmoto.com/wp-content/uploads/2018/12/10013231_1_file-10-11-2017-10-58-17-qFzv2Pud7cR-560x416.png",
+    },
+    {
+      id:7,
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPAzlz7bFMtDbBMGY6BXrzXOmsqRWgzPtjkg&usqp=CAU",
+    },
+    {
+      id:8,
+      img:"https://http2.mlstatic.com/D_NQ_NP_2X_928645-MLA45132741324_032021-F.webp",
+    },
+    {
+      id:9,
+      img:"https://cdn.shopify.com/s/files/1/0269/0750/8816/products/alpinestars-tech-8-rs-2015-ARBN_480x480.jpg?v=1588303576",
+    },
+    {
+      id:10,
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuVHBVQHAQYM7gxTahhjGKeMgnyd-iOLtzIg&usqp=CAU",
+    },
+    {
+      id:11,
+      img:"http://http2.mlstatic.com/D_833873-MLA48196764511_112021-O.jpg",
+    }
+  ]
